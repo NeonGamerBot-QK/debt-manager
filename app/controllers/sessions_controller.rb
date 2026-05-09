5036d85160953c1a2791fe246ac2561e07d20720
Neon <neon@saahild.com>
2026-05-09 18:01:52 -0400

    update(session): link session code -> user db model
    Signed-off-by: Neon <neon@saahild.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < ApplicationController
   def create
-  auth = request.env['omniauth.auth']
-  session[:user] = {
-    uid: auth['uid'],
-    email: auth['info']['email'],
-    name: auth['info']['name'],
-  }
-  redirect_to root_path, notice: "Signed in as #{session[:user][:name]}"
+    auth = request.env['omniauth.auth']
+    return redirect_to root_path, alert: "Authentication failed" unless auth
+
+    user = User.find_or_create_by(slack_id: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Signed in as #{user.name}"
   end
 
   def destroy
-    session[:user] = nil
+    reset_session
     redirect_to root_path, notice: "Signed out"
   end
 end
