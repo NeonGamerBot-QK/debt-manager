41eb502b509e2082ae2eeb2f0077153bc3c29fd9
Neon <neon@saahild.com>
2026-05-29 23:15:29 -0400

    update: more workrkrkrkrk
    Signed-off-by: Neon <neon@saahild.com>

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def create_project
+  end
+end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,12 @@ class ProjectsController < ApplicationController
 def create
   @project = Project.new 
   @project.name = params[:name]  
+  @project.description = params[:description]
+  @project.user = current_user
+  if @project.save
+    redirect_to dash_path, notice: "Project created successfully"
+  else
+    redirect_to dash_path, alert: "Failed to create project: #{@project.errors.full_messages.join(", ")}"
+end
+
 end
