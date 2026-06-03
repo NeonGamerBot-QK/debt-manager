cf97ae62bc0f9ef598f163d8ba875f26e000d428
Neon <neon@saahild.com>
2026-06-02 23:25:51 -0400

    update: create proj page lowk
    Signed-off-by: Neon <neon@saahild.com>

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,19 @@
 class ProjectsController < ApplicationController
+def new
+  @project = Project.new
+end
+
 def create
   @project = Project.new 
-  @project.name = params[:name]  
-  @project.description = params[:description]
+  @project.name = params[:name]
+  # @project.description = params[:description]
+  @project.hackatime_projects = params[:hackatime_projects].to_s.split(",").map(&:strip).reject(&:empty?)
   @project.user = current_user
   if @project.save
     redirect_to dash_path, notice: "Project created successfully"
   else
     redirect_to dash_path, alert: "Failed to create project: #{@project.errors.full_messages.join(", ")}"
+  end
 end
 
 end
